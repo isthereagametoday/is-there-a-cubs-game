0c1f0b302bad59818b9307c78143fd662d2a7b00
Mica Alaniz <mica.alaniz@gmail.com>
2016-03-18 10:33:41 -0500

    figuring out the twitter thing

--- twitter.js
+++ twitter.js
@@ -1,25 +1,42 @@
 #! /app/.heroku/node/bin/node
 
 var Twitter = require('twitter');
-var status = require('./server');
+var axios = require('axios');
+var moment = require('moment');
+var tz = require('moment-timezone');
 require('dotenv').config();
 
 function tweetStatus() {
-  console.log(status);
-  console.log('Hello');
-
-  var gameStatus;
-  if (status.length === 1) {
-    gameStatus = 'Yes at' + status.eventTime;
-  } else if (status.length > 1) {
-    gameStatus = 'Yes. There are actually several games today.';
-  } else {
-    gameStatus = 'No.';
+  function getToday() {
+    return moment().tz('America/Chicago').format().substr(0, 10);
   }
+  
+  var now = getToday();
+
+  function getEvents() {
+    return axios.get(`/api/events/${now}`);
+  }
+  var gameStatus = getEvents();
+  
+  var status = gameStatus.then(function (date) {
+    var data = date.data;
+    if (data.length === 1) {
+      return 'Yes at' + status.eventTime;
+    } else if (data.length > 1) {
+      return 'Yes. There are actually several games today.';
+    }
+    return 'No.';
+  });
+
+  console.log('gs:', gameStatus);
+  console.log("now:", now);
+  console.log('Hello');
+  console.log('status:', status);
 
   var params = {
-    status: gameStatus,
+    status: status,
   };
+
   var client = new Twitter({
     consumer_key: process.env.TWITTER_CONSUMER_KEY,
     consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
